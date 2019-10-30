3593b4c58c8c2c8f31ba1045dfa7d025069f0224
TomyJPN <masaya120331@gmail.com>
2019-10-30 22:05:37 +0900

    タイトル，クレジット等完成

---
+++ AR_Kosen/Assets/SwitchTerrain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchTerrain : MonoBehaviour {
+  [SerializeField]
+  MeshRenderer mesh;
+
+  [SerializeField]
+  Terrain terrain1;
+
+  [SerializeField]
+  Terrain terrain2;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (mesh.enabled) {
+      terrain1.enabled = true;
+      terrain2.enabled = true;
+    }
+    else {
+      terrain1.enabled = false;
+      terrain2.enabled = false;
+    }
+  }
+
+  public void onExitBtn() {
+    SceneManager.LoadScene("Title");
+  }
+}
---
+++ AR_Kosen/Assets/Title.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+  [SerializeField]
+  GameObject MarkerCanvas;
+
+  [SerializeField]
+  GameObject CreditCanvas;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  public void onMarkerBtn() {
+    MarkerCanvas.SetActive(true);
+  }
+  public void onMarkerExit() {
+    MarkerCanvas.SetActive(false);
+  }
+  public void onCreditBtn() {
+    CreditCanvas.SetActive(true);
+  }
+  public void onCreditExit() {
+    CreditCanvas.SetActive(false);
+  }
+  public void onStartBtn() {
+    SceneManager.LoadScene("MainScene");
+  }
+}
